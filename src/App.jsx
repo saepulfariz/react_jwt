71e239786d7aea7046e90ce8406c85ed4397794b
saepulfariz <saepulfariz108@gmail.com>
2024-08-07 18:18:42 +0700

    add middleware not fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import AuthMiddleware from "./middleware/authMiddleware";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route
+          path="/dashboard"
+          element={
+            <AuthMiddleware roles={["admininistrator"]}>
+              <Dashboard />
+            </AuthMiddleware>
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+    Navigate,
+    useNavigate
+} from 'react-router-dom';
+
+const API_URL =
+    import.meta.env.VITE_API_URL;
+
+const AuthMiddleware = ({
+    children,
+    roles
+}) => {
+    const token = localStorage.getItem('accessToken');
+    const [userRole, setUserRole] = React.useState(null);
+    const navigate = useNavigate();
+
+    React.useEffect(() => {
+        if (token) {
+            fetch(API_URL + '/api/me', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    },
+                })
+                .then(response => response.json())
+                .then(data => {
+                    setUserRole(data.role);
+                })
+                .catch(error => {
+                    console.error('Error fetching user role:', error);
+                    setUserRole(null);
+                });
+        } else {
+            setUserRole(null);
+        }
+    }, [token]);
+
+
+
+    if (!token) {
+        // return <Navigate to = "/" / > ;
+        return navigate("/");
+    }
+
+    if (userRole && roles && !roles.includes(userRole)) {
+        // return <Navigate to = "/dashboard" / > ;
+        return navigate("/");
+    }
+
+    return children;
+};
+
+export default AuthMiddleware;
